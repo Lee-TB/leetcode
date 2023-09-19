8b9b0823d0e365d166f8bd0f4c9340b7d6792a49
IndieDev <tblyduc2412@gmail.com>
2023-09-19 22:37:25 +0700

    287. Find the Duplicate Number

---
+++ csharp/287. Find the Duplicate Number/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _287._Find_the_Duplicate_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[] { 1, 3, 4, 2, 2 };
+            Console.WriteLine(FindDuplicate(nums));
+        }
+
+        public static int FindDuplicate(int[] nums)
+        {
+            List<int> list = new List<int>();
+            foreach (var num in nums)
+            {
+                if (list.Contains(num))
+                {
+                    return num;
+                }
+                list.Add(num);
+            }
+            return 0;
+        }
+    }
+}
