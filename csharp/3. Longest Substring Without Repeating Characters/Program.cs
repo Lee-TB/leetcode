30615bff788cd68b130ed0e542543d1270c7607b
IndieDev <tblyduc2412@gmail.com>
2023-09-24 20:30:31 +0700

    3. Longest Substring Without Repeating Characters

---
+++ csharp/3. Longest Substring Without Repeating Characters/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace _3._Longest_Substring_Without_Repeating_Characters;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(LengthOfLongestSubstring("abccbcbb"));
+    }
+    public static int LengthOfLongestSubstring(string s)
+    {
+        StringBuilder substring = new StringBuilder();
+        int longestLength = 0;
+
+        foreach (var c in s)
+        {
+            int charRepeatIndex = substring.ToString().IndexOf(c);
+            if (charRepeatIndex != -1)
+            {                
+                substring = new StringBuilder(substring.ToString().Substring(charRepeatIndex + 1));
+            }
+ 
+            substring.Append(c);
+            longestLength = Math.Max(substring.Length, longestLength);
+        }
+
+        return longestLength;
+    }
+}
