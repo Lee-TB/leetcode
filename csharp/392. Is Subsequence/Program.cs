cfa2116bacd249cc0523dd7e95ce418dfe1dad8f
IndieDev <tblyduc2412@gmail.com>
2023-09-24 14:28:21 +0700

    392. Is Subsequence

---
+++ csharp/392. Is Subsequence/Program.cs
@@ -0,0 +1,21 @@
+﻿// Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+namespace _392._Is_Subsequence;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(IsSubsequence("ace", "abcde"));
+        
+    }
+    public static bool IsSubsequence(string s, string t)
+    {
+        if (s.Length == 0) return true;       
+
+        if (s.Length > t.Length) return false;
+
+        int index = t.IndexOf(s[0]);
+        if ( t.IndexOf(s[0]) == -1) return false;        
+
+        return IsSubsequence(s.Substring(1), t.Substring(index + 1));        
+    }
+}
