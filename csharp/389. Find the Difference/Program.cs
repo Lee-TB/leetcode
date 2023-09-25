bd02df8ca5a450eefc57c38e0172add8cf48346b
IndieDev <tblyduc2412@gmail.com>
2023-09-25 20:10:31 +0700

    389. Find the Difference

---
+++ csharp/389. Find the Difference/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _389._Find_the_Difference;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(FindTheDifference("avx", "asvx"));
+    }
+
+    public static char FindTheDifference_Simple(string s, string t)
+    {
+        int result = 0;
+
+        foreach (char c in t) result += c;
+
+        foreach (char c in s) result -= c;
+
+        return (char)result;
+    }
+
+    public static char FindTheDifference(string s, string t)
+    {   
+        Dictionary<char, int> map = new Dictionary<char, int>();
+
+        for (int i = 0; i < t.Length; i++)
+        {
+            if (map.ContainsKey(t[i]))
+            {
+                map[t[i]]++;
+            }
+            else
+            {
+                map[t[i]] = 1;
+            }
+        }
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (map.ContainsKey(s[i]))
+            {
+                map[s[i]]--;
+            }
+        }
+
+        foreach (char key in map.Keys) {
+            if (map[key] == 1)
+            {
+                return key;
+            }
+        }
+
+        return 'N';
+
+    }
+}
