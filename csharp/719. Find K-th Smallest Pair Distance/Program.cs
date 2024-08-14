8e4ae227c1deba5ce37d585ca4416c43b8f38605
Tommy Tran <tblyduc2412@gmail.com>
2024-08-14 19:26:49 +0700

    719. Find K-th Smallest Pair Distance719. Find K-th Smallest Pair Distance

---
+++ csharp/719. Find K-th Smallest Pair Distance/Program.cs
@@ -0,0 +1,32 @@
+﻿var sln = new Solution();
+Console.WriteLine(sln.SmallestDistancePair([1, 6, 1], 3));
+
+public class Solution
+{
+    public int SmallestDistancePair(int[] nums, int k)
+    {
+        int arrLen = nums.Length;
+
+        int maxValue = int.MinValue;
+        foreach (int num in nums)
+        {
+            maxValue = Math.Max(maxValue, num);
+        }
+
+        int[] distanceBucket = new int[maxValue + 1];
+        for (int i = 0; i < arrLen; i++)
+            for (int j = i + 1; j < arrLen; j++)
+            {
+                int distance = Math.Abs(nums[i] - nums[j]);
+                ++distanceBucket[distance];
+            }
+
+        for (int dist = 0; dist <= maxValue; ++dist)
+        {
+            k -= distanceBucket[dist];
+            if (k <= 0) return dist;
+        }
+
+        return -1;
+    }
+}
