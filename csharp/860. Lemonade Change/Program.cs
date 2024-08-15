cffc21ad84288896dbac894459ec582f962a1e09
Tommy Tran <tblyduc2412@gmail.com>
2024-08-15 20:20:13 +0700

    860. Lemonade Change860. Lemonade Change

---
+++ csharp/860. Lemonade Change/Program.cs
@@ -0,0 +1,43 @@
+﻿var sln = new Solution();
+int[] bills = [5, 5, 5, 10, 20];
+Console.WriteLine(sln.LemonadeChange(bills));
+int[] bills2 = [5, 5, 10, 10, 20];
+Console.WriteLine(sln.LemonadeChange(bills2));
+
+public class Solution
+{
+    public bool LemonadeChange(int[] bills)
+    {
+        int fiveBill = 0;
+        int tenBill = 0;
+        foreach (var bill in bills)
+        {
+            if (bill == 5)
+                fiveBill++;
+            else if (bill == 10)
+            {
+                if (fiveBill >= 1)
+                {
+                    tenBill++;
+                    fiveBill--;
+
+                }
+                else return false;
+            }
+            else
+            {
+                if (tenBill >= 1 && fiveBill >= 1)
+                {
+                    tenBill--;
+                    fiveBill--;
+                }
+                else if (fiveBill >= 3)
+                {
+                    fiveBill -= 3;
+                }
+                else return false;
+            }
+        }
+        return true;
+    }
+}
