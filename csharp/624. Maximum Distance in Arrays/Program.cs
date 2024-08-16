cf286a29fe4ab0cd251157cbe70ad5bb2d96e392
Tommy Tran <tblyduc2412@gmail.com>
2024-08-16 20:37:59 +0700

    624. Maximum Distance in Arrays

---
+++ csharp/624. Maximum Distance in Arrays/Program.cs
@@ -0,0 +1,20 @@
+﻿var sln = new Solution();
+int[][] arrays = [[1, 4], [0, 5]];
+Console.WriteLine(sln.MaxDistance(arrays));
+public class Solution
+{
+    public int MaxDistance(IList<IList<int>> arrays)
+    {        
+        int maxDistance = 0, minValue = arrays[0].Min(), maxValue = arrays[0].Max();
+
+        for (int i = 1; i < arrays.Count; i++)
+        {
+            maxDistance = Math.Max(maxDistance, Math.Abs(maxValue - arrays[i].Min()));
+            maxDistance = Math.Max(maxDistance, Math.Abs(minValue - arrays[i].Max()));
+            minValue = Math.Min(arrays[i].Min(), minValue);
+            maxValue = Math.Max(arrays[i].Max(), maxValue);
+        }
+
+        return maxDistance;
+    }
+}
