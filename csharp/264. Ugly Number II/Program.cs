1b4dce939f97d832b9a1b2ad28dd4f224d820045
Tommy Tran <tblyduc2412@gmail.com>
2024-08-18 20:18:35 +0700

    264. Ugly Number II

---
+++ csharp/264. Ugly Number II/Program.cs
@@ -0,0 +1,22 @@
+﻿var sln = new Solution();
+Console.WriteLine(sln.NthUglyNumber(1690));
+
+public class Solution
+{
+    public int NthUglyNumber(int n)
+    {
+        HashSet<long> set = new() { 1 };
+        long currentUgly = 1;
+
+        for (int i = 0; i < n; i++)
+        {
+            currentUgly = set.Min();
+            set.Remove(currentUgly);
+            set.Add(currentUgly * 2);
+            set.Add(currentUgly * 3);
+            set.Add(currentUgly * 5);            
+        }
+        
+        return (int)currentUgly;
+    }
+}
