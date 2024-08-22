5a0a4ccc2ffd39e9ec7e8d383bf492178cf7e7d2
Tommy Tran <tblyduc2412@gmail.com>
2024-08-22 19:24:52 +0700

    476. Number Complement

---
+++ csharp/476. Number Complement/Program.cs
@@ -0,0 +1,15 @@
+﻿var sln = new Solution();
+
+Console.WriteLine(sln.FindComplement(5)); // 2
+public class Solution
+{
+    public int FindComplement(int num)
+    {
+        // get the length of the binary representation of the number
+        int bitLen = Convert.ToString(num, toBase: 2).Length;
+
+        int mask = (1 << bitLen) - 1; // if bitLen = 3, mask = 1000 - 1 = 111
+
+        return mask ^ num;
+    }
+}
