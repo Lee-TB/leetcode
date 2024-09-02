bf30228eaf90dadecd28070441d8cb3c99a73808
Tommy Tran <tblyduc2412@gmail.com>
2024-09-02 23:03:46 +0700

    1894. Find the Student that Will Replace the Chalk

---
+++ csharp/1894. Find the Student that Will Replace the Chalk/Program.cs
@@ -0,0 +1,26 @@
+﻿var sln = new Solution();
+int[] chalk = [5, 1, 5];
+int k = 22;
+Console.WriteLine(sln.ChalkReplacer(chalk, k));
+
+public class Solution
+{
+    public int ChalkReplacer(int[] chalk, int k)
+    {
+        long sumChalk = 0;
+        foreach (var c in chalk)
+        {
+            sumChalk += c;
+        }
+        int moduloChalk = (int)(k % sumChalk); // find minimum Chalk need to use to reduce time
+
+        int i = 0;
+        while (moduloChalk >= chalk[i])
+        {
+            moduloChalk -= chalk[i];
+            i++;
+            if (i >= chalk.Length) i = 0;
+        }
+        return i;
+    }
+}
