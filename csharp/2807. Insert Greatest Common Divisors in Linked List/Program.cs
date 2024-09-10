1c1831f915f82f8b8a15203b7c07bad530c02f6b
Tommy Tran <tblyduc2412@gmail.com>
2024-09-10 10:33:46 +0700

    2807. Insert Greatest Common Divisors in Linked List.

---
+++ csharp/2807. Insert Greatest Common Divisors in Linked List/Program.cs
@@ -0,0 +1,32 @@
+﻿using Leetcode.DataStructure;
+using Leetcode.Utils;
+
+int[] nums = [18, 6, 10, 3];
+ListNode head = Converter.ArrayToListNode(nums);
+
+var sln = new Solution();
+ListNode res = sln.InsertGreatestCommonDivisors(head);
+Printer.PrintListNode(res);
+public class Solution
+{
+    public ListNode InsertGreatestCommonDivisors(ListNode head)
+    {
+        ListNode currentNode = head;
+        while (currentNode.next != null)
+        {
+            // find gcd
+            int gcd = GCD(currentNode.val, currentNode.next.val);
+            // create new node and insert it into linked list.
+            ListNode newNode = new ListNode(gcd, currentNode.next);
+            currentNode.next = newNode;
+            currentNode = currentNode.next.next; // next two time to skip newNode
+        }
+        return head;
+    }
+
+    private int GCD(int a, int b)
+    {
+        if (b == 0) return a;
+        return GCD(b, a % b);
+    }
+}
---
+++ csharp/Leetcode/Utils/Printer.cs
@@ -0,0 +1,20 @@
+﻿using Leetcode.DataStructure;
+
+namespace Leetcode.Utils;
+
+public static class Printer
+{
+    public static void PrintListNode(ListNode head)
+    {
+        Console.Write("[");
+        while (head != null)
+        {
+            if (head.next == null)
+                Console.Write(head.val);
+            else
+                Console.Write(head.val + ", ");
+            head = head.next;
+        }
+        Console.WriteLine("]");
+    }
+}
