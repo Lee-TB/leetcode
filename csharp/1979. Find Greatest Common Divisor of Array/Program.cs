c03ccc05372b11fe6abdddfd72d5e3a8be9084f1
Tommy Tran <tblyduc2412@gmail.com>
2024-09-10 09:47:53 +0700

    1979. Find Greatest Common Divisor of Array

---
+++ csharp/1979. Find Greatest Common Divisor of Array/Program.cs
@@ -0,0 +1,36 @@
+﻿int[] nums = [2, 5, 6, 9, 10];
+var sln = new Solution();
+int gcd = sln.FindGCD(nums);
+
+Console.WriteLine(gcd);
+
+public class Solution
+{
+    public int FindGCD(int[] nums)
+    {
+        int minValue = 1000;
+        int maxValue = 1;
+        foreach (int num in nums)
+        {
+            minValue = Min(minValue, num);
+            maxValue = Max(maxValue, num);
+        }
+        return GCD(minValue, maxValue);
+    }
+
+    public int GCD(int a, int b)
+    {
+        if (b == 0) return a;
+        return GCD(b, a % b);
+    }
+
+    public int Min(int a, int b)
+    {
+        return a < b ? a : b;
+    }
+
+    public int Max(int a, int b)
+    {
+        return a > b ? a : b;
+    }
+}
