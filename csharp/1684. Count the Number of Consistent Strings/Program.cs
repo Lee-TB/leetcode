9402dee8a6eb11eb6d03715965e69456faf750c1
Tommy Tran <tblyduc2412@gmail.com>
2024-09-12 09:05:08 +0700

    1684. Count the Number of Consistent Strings

---
+++ csharp/1684. Count the Number of Consistent Strings/Program.cs
@@ -0,0 +1,24 @@
+﻿var sln = new Solution();
+string allowed = "abc";
+string[] words = ["a", "b", "c", "ab", "ac", "bc", "abc"];
+Console.WriteLine(sln.CountConsistentStrings(allowed, words));
+public class Solution
+{
+    public int CountConsistentStrings(string allowed, string[] words)
+    {
+        int count = 0;
+        foreach (string word in words)
+        {
+            count++;
+            foreach (char c in word)
+            {
+                if (!allowed.Contains(c))
+                {
+                    count--;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
