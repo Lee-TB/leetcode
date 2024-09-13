5219068547ed1d606a67c9582b00c288b42a1998
Tommy Tran <tblyduc2412@gmail.com>
2024-09-13 09:45:42 +0700

    1310. XOR Queries of a Subarray

---
+++ csharp/1310. XOR Queries of a Subarray/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] arr = [1, 3, 4, 8];
+int[][] queries = [[0, 1], [1, 2], [0, 3], [3, 3]];
+
+var sln = new Solution();
+var res = sln.XorQueries(arr, queries);
+
+foreach (int i in res)
+{
+    Console.Write(i + " ");
+}
+
+public class Solution
+{
+    public int[] XorQueries(int[] arr, int[][] queries)
+    {
+        int[] ans = new int[queries.Length];
+        int i = 0;
+        foreach (var query in queries)
+        {
+            int xor = 0;
+            for (int j = query[0]; j <= query[1]; j++)
+            {
+                xor ^= arr[j];                
+            }
+            ans[i++] = xor;
+        }
+        return ans;
+    }
+}
