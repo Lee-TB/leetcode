48c04f3590d6f311c61a8370add6d8d03a260435
Tommy Tran <tblyduc2412@gmail.com>
2024-09-14 12:40:28 +0700

    53. Maximum Subarray

---
+++ csharp/53. Maximum Subarray/Program.cs
@@ -0,0 +1,20 @@
+﻿var sln = new Solution();
+var res = sln.MaxSubArray([-2, 1, -3, 4, -1, 4, 1, -5, 4]);
+Console.WriteLine(res);
+public class Solution
+{
+    public int MaxSubArray(int[] nums)
+    {
+        int maxSum = nums[0], sum = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+            if (sum < nums[i])
+            {
+                sum = nums[i];
+            }
+            maxSum = sum > maxSum ? sum : maxSum;
+        }
+        return maxSum;
+    }
+}
