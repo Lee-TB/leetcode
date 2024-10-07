358e994ac8d1dd4c865d86870a777a60bac7efe3
Tommy Tran <tblyduc2412@gmail.com>
2024-10-07 11:46:42 +0700

    2696. Minimum String Length After Removing Substrings (csharp)

---
+++ csharp/2696. Minimum String Length After Removing Substrings/Program.cs
@@ -0,0 +1,7 @@
+﻿using _2696._Minimum_String_Length_After_Removing_Substrings;
+
+Console.WriteLine("2696. Minimum String Length After Removing Substrings");
+string s = "ABFCACDB";
+var sln = new Solution();
+int min = sln.MinLength(s);
+Console.WriteLine(min);
---
+++ csharp/2696. Minimum String Length After Removing Substrings/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace _2696._Minimum_String_Length_After_Removing_Substrings;
+
+public class Solution
+{
+    public int MinLength(string s)
+    {
+        while (s.Contains("AB") || s.Contains("CD")) {            
+            if(s.Contains("AB"))
+            {
+                s = s.Replace("AB", "");
+            }
+            if(s.Contains("CD"))
+            {
+                s = s.Replace("CD", "");
+            }
+        }
+        return s.Length;
+    }
+}
