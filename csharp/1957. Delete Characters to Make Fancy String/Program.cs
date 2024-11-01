81ac28a89381a69ab96549b41081cd038250f968
Tommy Tran <tblyduc2412@gmail.com>
2024-11-01 19:20:58 +0700

    1957. Delete Characters to Make Fancy String

---
+++ csharp/1957. Delete Characters to Make Fancy String/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _1957._Delete_Characters_to_Make_Fancy_String;
+
+var sln = new Solution();
+string s = "abbcccddddeeeee";
+var res = sln.MakeFancyString(s);
+Console.WriteLine(res);
+
---
+++ csharp/1957. Delete Characters to Make Fancy String/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace _1957._Delete_Characters_to_Make_Fancy_String;
+
+public class Solution
+{
+    public string MakeFancyString(string s)
+    {
+        StringBuilder sb = new StringBuilder();
+        char prevChar = s[0];
+        int count = 0;
+        foreach (char c in s)
+        {
+            if (c == prevChar)
+            {
+                count++;
+            } else
+            {
+                count = 1;
+            }
+
+            if(count <= 2)
+            {
+                sb.Append(c);                        
+            }
+            prevChar = c;
+        }
+        return sb.ToString();
+    }
+}
