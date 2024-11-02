13788e6797bb90e9f960230ebe012519cbf2dcb4
Tommy Tran <tblyduc2412@gmail.com>
2024-11-02 14:07:15 +0700

    2490. Circular Sentence (csharp)

---
+++ csharp/2490. Circular Sentence/Program.cs
@@ -0,0 +1,7 @@
+﻿using _2490._Circular_Sentence;
+
+Console.WriteLine("2490. Circular Sentence");
+var sln = new Solution();
+var sentence = "leetcode exercises sound delightful";
+var res = sln.IsCircularSentence(sentence);
+Console.WriteLine(res);
---
+++ csharp/2490. Circular Sentence/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace _2490._Circular_Sentence;
+
+public class Solution
+{
+    public bool IsCircularSentence(string sentence)
+    {
+        var words = sentence.Split(" ");
+
+        // check beetween two adjacent words
+        for (int i = 1; i < words.Length; i++)
+        {
+            var prevWord = words[i - 1];
+            var currWord = words[i];
+            if (prevWord[prevWord.Length - 1] != currWord[0])
+            {
+                return false;
+            }
+        }
+
+        // check first and last words
+        var firstWord = words[0];
+        var lastWord = words[words.Length - 1];
+        if (firstWord[0] != lastWord[lastWord.Length - 1])
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
