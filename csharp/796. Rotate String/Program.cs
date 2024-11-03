e7a35170b81465f5c7f48ad4619a33dc09b80163
Tommy Tran <tblyduc2412@gmail.com>
2024-11-03 11:43:23 +0700

    796. Rotate String (csharp)

---
+++ csharp/796. Rotate String/Program.cs
@@ -0,0 +1,11 @@
+﻿using _796._Rotate_String;
+
+Console.WriteLine("796. Rotate String");
+var sln = new Solution();
+string s1 = "abcde", goal1 = "cdeab";
+var res1 = sln.RotateString(s1, goal1);
+Console.WriteLine("result 1: " + res1);
+
+string s2 = "abcde", goal2 = "abced";
+var res2 = sln.RotateString(s2, goal2);
+Console.WriteLine("result 2: " + res2);
---
+++ csharp/796. Rotate String/Solution.cs
@@ -0,0 +1,11 @@
+﻿namespace _796._Rotate_String;
+
+public class Solution
+{
+    public bool RotateString(string s, string goal)
+    {
+        if(s.Length != goal.Length) return false;
+        string concatenatedString = s + s;
+        return concatenatedString.Contains(goal);
+    }
+}
