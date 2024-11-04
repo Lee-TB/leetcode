332a48825caa4ac057585b39a97c3a4f54203b03
Tommy Tran <tblyduc2412@gmail.com>
2024-11-04 11:08:27 +0700

    3163. String Compression III (csharp)

---
+++ csharp/3163. String Compression III/Program.cs
@@ -0,0 +1,11 @@
+﻿using _3163._String_Compression_III;
+
+Console.WriteLine("3163. String Compression III");
+var sln = new Solution();
+string word1 = "abcde";
+var result1 = sln.CompressedString(word1);
+Console.WriteLine("result 1: " + result1);
+
+string word2 = "aaaaaaaaaaaaaabb";
+var result2 = sln.CompressedString(word2);
+Console.WriteLine("result 2: " + result2);
---
+++ csharp/3163. String Compression III/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+namespace _3163._String_Compression_III;
+
+public class Solution
+{
+    public string CompressedString(string word)
+    {
+        StringBuilder sb = new StringBuilder();
+        char currentChar = word[0];
+        int times = 0;
+        foreach (char c in word)
+        {
+            if (currentChar == c)
+            {
+                times++;
+                if (times == 9)
+                {
+                    sb.Append(times);
+                    sb.Append(currentChar);
+                    times = 0;
+                }
+            }
+            else
+            {
+                if(times !=0 )
+                {
+                    sb.Append(times);
+                    sb.Append(currentChar);
+                }
+                times = 1;
+                currentChar = c;
+            }
+        }
+
+        if (times != 0)
+        {
+            sb.Append(times);
+            sb.Append(currentChar);
+        }
+        return sb.ToString();
+    }
+}
