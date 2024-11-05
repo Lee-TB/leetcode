dafa16be8406ef35ba7858d4783fe6012a32bb16
Tommy Tran <tblyduc2412@gmail.com>
2024-11-05 19:25:18 +0700

    2914. Minimum Number of Changes to Make Binary String Beautiful (csharp)

---
+++ csharp/2914. Minimum Number of Changes to Make Binary String Beautiful/Program.cs
@@ -0,0 +1,7 @@
+﻿
+using _2914._Minimum_Number_of_Changes_to_Make_Binary_String_Beautiful;
+
+Console.WriteLine("2914. Minimum Number of Changes to Make Binary String Beautiful");
+var sln = new Solution();
+var res = sln.MinChanges("1000");
+Console.WriteLine(res);
---
+++ csharp/2914. Minimum Number of Changes to Make Binary String Beautiful/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace _2914._Minimum_Number_of_Changes_to_Make_Binary_String_Beautiful;
+
+public class Solution
+{
+    public int MinChanges(string s)
+    {
+        int minChanges = 0;
+        int count = 0;
+        char currentChar = s[0];
+        foreach (char c in s)
+        {
+            if(c == currentChar)
+            {
+                count++;
+                continue;
+            }
+
+            if (count % 2 == 0) { 
+                count = 1;
+            } else
+            {
+                minChanges++;
+                count++;
+            }
+
+            currentChar = c;
+        }
+
+        return minChanges;
+    }
+}
